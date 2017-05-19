9ce30439ff9f2ac0dce8873bce7b1b79f4646edb
mellaniesoriano <msoriano808@gmail.com>
2017-05-18 18:57:40 -1000

    test passed

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,15 @@
 'use strict';
 module.exports = function() {
-  
+
+  var secretNumber = Math.floor((Math.random() * 1000000) + 1);
+
+  return function(){
+    return secretNumber;
+  };
+
+  // function random(){
+  //   return secretNumber;
+  // }
+  // return random;
+
 };
